f0a39b1778b9f7a42ab649d47066b498d89cec43
fedorwk <fedorwk@gmail.com>
2022-03-29 23:17:42 +0300

    tests rewritten

---
+++ test/templater_test.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/fedorwk/templater"
+)
+
+var (
+	BasicTemplate    = "{objects} are {adjective}"
+	OneSidedTemplate = "$objects are $adjective"
+	BasicItems       = []map[string]string{
+		{"objects": "Roses", "adjective": "red"},
+		{"objects": "violets", "adjective": "blue"},
+	}
+	TextExpected = []string{
+		"Roses are red",
+		"violets are blue",
+	}
+)
+
+func TestBasicTemplateReplacer(t *testing.T) {
+	template := BasicTemplate
+	items := BasicItems
+	multireplacer := templater.NewMultiReplacer(items, "{", "}")
+	templater := templater.NewTemplater(template, multireplacer)
+	result := templater.AllStrings() // Get all strings with replacement by item data
+	for i := range result {
+		if result[i] != TextExpected[i] {
+			t.Errorf("unexpected result\nwant:%s, \ngot:%s", TextExpected[i], result[i])
+		}
+	}
+}
+
+func TestOneSidedTemplateReplacer(t *testing.T) {
+	template := OneSidedTemplate
+	items := BasicItems
+	multireplacer := templater.NewMultiReplacer(items, "$", "")
+	templater := templater.NewTemplater(template, multireplacer)
+	// Get strigns one by one with replacement by item data
+	for i := range items {
+		if result := templater.ExecuteToString(i); result != TextExpected[i] {
+			t.Errorf(
+				"unexpected result\nwant:%s, \ngot:%s",
+				TextExpected[i],
+				result)
+		}
+	}
+}
--- test/testUnits.go
+++ test/testUnits.go
@@ -6,20 +6,7 @@ import (
 	"os"
 )
 
-var BasicTemplate = "Hi, {name}! You look {adjective}!"
-
 var (
-	BasicItems = []map[string]string{
-		{"name": "Fedor", "adjective": "cool"},
-		{"name": "Kate", "adjective": "smart"},
-		{"name": "Sam", "adjective": "good"},
-	}
-	BasicItemsExpected = []string{
-		"Hi, Fedor! You look cool!",
-		"Hi, Kate! You look smart!",
-		"Hi, Sam! You look good!",
-	}
-
 	EmailItems = []map[string]string{
 		{"email": "exmpl@exmpl.com", "subject": "exmpl", "text": "some text"},
 		{"email": "one@one.ru", "subject": "onesubjext", "text": "new\nline\ntest"},
