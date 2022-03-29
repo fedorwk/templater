c1e69b1b2ba3b2ec5b8a4bfc5cb94bb375901925
fedorwk <fedorwk@gmail.com>
2022-03-29 22:40:30 +0300

    tests rewritten

--- test/endtoend_test.go
+++ test/endtoend_test.go
@@ -12,7 +12,12 @@ func TestBasicReplacer(t *testing.T) {
 	items := BasicItems
 	multireplacer := templater.NewMultiReplacer(items, "{", "}")
 	templater := templater.NewTemplater(template, multireplacer)
-	fmt.Println(templater.AllStrings())
+	result := templater.AllStrings()
+	for i := range result {
+		if result[i] != BasicItemsExpected[i] {
+			t.Errorf("unexpected result\nwant:%s, \ngot:%s", BasicItemsExpected[i], result[i])
+		}
+	}
 }
 
 func TestEmailReplacer(t *testing.T) {
--- test/testUnits.go
+++ test/testUnits.go
@@ -14,6 +14,11 @@ var (
 		{"name": "Kate", "adjective": "smart"},
 		{"name": "Sam", "adjective": "good"},
 	}
+	BasicItemsExpected = []string{
+		"Hi, Fedor! You look cool!",
+		"Hi, Kate! You look smart!",
+		"Hi, Sam! You look good!",
+	}
 
 	EmailItems = []map[string]string{
 		{"email": "exmpl@exmpl.com", "subject": "exmpl", "text": "some text"},
